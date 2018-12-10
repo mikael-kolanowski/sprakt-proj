98b80e52c5025f4cda9d3f9803bd9dddcd7ffd78
Amanda Kann <akane@kth.se>
2018-12-10 12:24:18 +0100

    progress...

--- RI/random_indexing.py
+++ RI/random_indexing.py
@@ -5,13 +5,15 @@ import string
 import numpy as np
 import tempfile
 from halo import Halo
+from itertools import combinations
+from scipy.spatial.distance import cosine
 from sklearn.neighbors import NearestNeighbors
 
 
 class RandomIndexing(object):
-    def __init__(self, filenames, dimension=2000, non_zero=100, non_zero_values=[-1, 1], left_window_size=3, right_window_size=3):
+    def __init__(self, filenames, dimension=2000, non_zero=100, non_zero_values=[-1, 1], left_window_size=6, right_window_size=6):
         self.__sources = filenames
-        self.__vocab = set()
+        self.vocab = set()
         self.__dim = dimension
         self.__non_zero = non_zero
         self.__non_zero_values = non_zero_values
@@ -19,7 +21,6 @@ class RandomIndexing(object):
         self.__rws = right_window_size
         self.__cv = None
         self.__rv = None
-
 
     def clean_line(self, line):
         import string
@@ -44,22 +45,22 @@ class RandomIndexing(object):
         linegen = self.text_gen()
         for line in linegen:
             for word in line:
-                self.__vocab.add(word)
+                self.vocab.add(word)
         self.write_vocabulary()
 
 
     @property
     def vocabulary_size(self):
-        return len(self.__vocab)
+        return len(self.vocab)
 
 
     def create_word_vectors(self):
         """
         Create word embeddings using Random Indexing
         """
-        self.__cv = {word:np.zeros(self.__dim) for word in self.__vocab}
-        self.__rv = {word:np.zeros(self.__dim) for word in self.__vocab}
-        for word in self.__vocab:
+        self.__cv = {word:np.zeros(self.__dim) for word in self.vocab}
+        self.__rv = {word:np.zeros(self.__dim) for word in self.vocab}
+        for word in self.vocab:
             randindex = np.random.choice(self.__dim - 1, self.__non_zero, replace=False)
             for ind in randindex:
                 self.__rv[word][ind] = np.random.choice(self.__non_zero_values)
@@ -71,11 +72,11 @@ class RandomIndexing(object):
                 cumul = np.zeros(self.__dim)
                 for n in range(self.__lws):
                     if i - n - 1 >= 0:
-                        cumul = np.add(self.__rv[words[i - n - 1]], cumul)
+                        cumul = (self.__rv[words[i - n - 1]]) / (n + 1) + cumul
                 for n in range(self.__rws):
                     if i + n + 1 <= len(words) - 1:
-                        cumul = np.add(self.__rv[words[i + n + 1]], cumul)
-                self.__cv[word] = np.add(self.__cv[word], cumul)
+                        cumul = (self.__rv[words[i + n + 1]]) / (n + 1) + cumul
+                self.__cv[word] = self.__cv[word] + cumul
         pass
 
 
@@ -106,7 +107,7 @@ class RandomIndexing(object):
         """
         Returns a trained vector for the word
         """
-        if word in self.__vocab:
+        if word in self.vocab:
             return self.__cv[word]
         return None
 
@@ -114,7 +115,7 @@ class RandomIndexing(object):
     def write_vocabulary(self):
         with tempfile.NamedTemporaryFile('w') as f:
             self.tempname = f.name
-            for w in self.__vocab:
+            for w in self.vocab:
                 f.write('{}\n'.format(w))
 
 
@@ -143,7 +144,28 @@ class RandomIndexing(object):
             text = input('> ')
 
 
+def compare_distance(word1, word2, ris):
+    output = []
+    for mdl in ris:
+        v1 = mdl.get_word_vector(word1)
+        v2 = mdl.get_word_vector(word2)
+        output.append(cosine(v1, v2))
+    return output
 
+def automatic_testing(models):
+    pop_size = 500
+    common = set.intersection(*[model.vocab for model in models.values()])
+    distances = []
+    sample = np.random.choice(list(common), pop_size)
+    for i in range(int(pop_size / 2)):
+        index1, index2 = np.random.randint(pop_size, size=2)
+        distances.append(compare_distance(sample[index1], sample[index2], models.values()))
+    for comb in combinations(models.keys(), r=2):
+        distlist = []
+        for distance in distances:
+            name_to_index = {name:i for i, name in enumerate(models.keys())}
+            distlist.append(abs(distance[name_to_index[comb[0]]] - distance[name_to_index[comb[1]]]))
+        print('Average distance between {} and {}: {}'.format(comb[0], comb[1], np.mean(distlist)))
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Random Indexing word embeddings')
@@ -172,13 +194,20 @@ if __name__ == '__main__':
         text = input('> ')
         while text != '':
             text = text.split()
-            for model_name, ri in ri_dict.items():
-                neighbors = ri.find_nearest(text)
-                if neighbors:
-                    for w, n in zip(text, neighbors):
-                        #print("Neighbors for {} in {}: {}".format(w, model_name, n))
-                        print('Neighbors for {} in {}'.format(w, model_name))
-                        for i, pair in enumerate(n):
-                            print(str(i+1) + " " + str(pair[0]) + "\t\t\t" + str(pair[1]))
-                    print('')
+            if text[0] == 'compare':
+                dist = compare_distance(text[1], text[2], ri_dict.values())
+                for i, mdl in enumerate(ri_dict.keys()):
+                    print('Distance between {} and {} in {}: {}'.format(text[1], text[2], mdl, dist[i]))
+            elif text[0] == 'autotest':
+                automatic_testing(ri_dict)
+            else:
+                for model_name, ri in ri_dict.items():
+                    neighbors = ri.find_nearest(text)
+                    if neighbors:
+                        for w, n in zip(text, neighbors):
+                            #print("Neighbors for {} in {}: {}".format(w, model_name, n))
+                            print('Neighbors for {} in {}'.format(w, model_name))
+                            for i, pair in enumerate(n):
+                                print(str(i+1) + " " + str(pair[0]) + "\t\t\t" + str(pair[1]))
+                        print('')
             text = input('> ')
